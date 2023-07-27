9628f2c5fb339edb82e1f5e1e50828dbc5f3b7df
Dubey9 <ajaydubey6245@gmail.com>
2023-07-27 17:45:49 +0700

    first commit

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> 7006443697 </label>
+            <i className='fa fa-envelope'></i>
+            <label> ajaydubey6245@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label> Feedback </label>
+            <label>Need Help?</label>
+            <span>🏳️‍⚧️</span>
+            <label>EN</label>
+            <span>🏳️‍⚧️</span>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
